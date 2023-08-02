afa8cb9393a1004f05182b013bef8ec9c426ce58
Shihan Nuwanjith <shihannuwanjith@gmail.com>
2023-08-03 00:29:51 +0530

    create Dashboard Controller class

---
+++ src/com/seekerscloud/pos/controller/DashboardFormController.java
@@ -0,0 +1,22 @@
+package com.seekerscloud.pos.controller;
+
+import javafx.event.ActionEvent;
+
+public class DashboardFormController {
+    private void setui(String ui){
+
+    }
+
+    public void openCustomerFormOnAction(ActionEvent actionEvent) {
+    }
+
+    public void openItemFormOnAction(ActionEvent actionEvent) {
+    }
+
+    public void openPlaceOrderFormOnAction(ActionEvent actionEvent) {
+    }
+
+    public void openOrderDetailsFormOnAction(ActionEvent actionEvent) {
+
+    }
+}
