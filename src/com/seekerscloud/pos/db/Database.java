1db3537f26026bd20db04518cc0205c18888eeec
Shihan Nuwanjith <shihannuwanjith@gmail.com>
2023-08-03 09:25:30 +0530

    implement the local data base

--- src/com/seekerscloud/pos/db/Database.java
+++ src/com/seekerscloud/pos/db/Database.java
@@ -1,4 +1,9 @@
 package com.seekerscloud.pos.db;
 
+import com.seekerscloud.pos.model.Customer;
+
+import java.util.ArrayList;
+
 public class Database {
+    public static ArrayList<Customer> customerTable = new ArrayList<>();
 }
