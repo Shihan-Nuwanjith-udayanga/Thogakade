efc2480bef8d9708a08c897cdec2a39c87539f90
Shihan Nuwanjith <shihannuwanjith@gmail.com>
2023-08-03 09:26:52 +0530

    create saveCustomerOnAction Method

--- src/com/seekerscloud/pos/controller/CustomerFormController.java
+++ src/com/seekerscloud/pos/controller/CustomerFormController.java
@@ -1,8 +1,12 @@
 package com.seekerscloud.pos.controller;
 
+import com.seekerscloud.pos.db.Database;
+import com.seekerscloud.pos.model.Customer;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
@@ -17,16 +21,28 @@ public class CustomerFormController {
     public TextField txtAddress;
     public TextField txtSalary;
 
-    private void setUi(String ui) throws IOException {
-        Stage stage = (Stage) customerFormContext.getScene().getWindow();
-        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("../view/"+ui+".fxml"))));
+    public void initialize(){
     }
 
-    public void saveCustomerOnAction(ActionEvent actionEvent) {
-
+    private void setUi(String ui) throws IOException {
+        Stage stage = (Stage) customerFormContext.getScene().getWindow();
+        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("../view/" + ui + ".fxml"))));
     }
 
     public void btnBackToHomeOnAction(ActionEvent actionEvent) throws IOException {
         setUi("DashboardForm");
     }
+
+    public void saveCustomerOnAction(ActionEvent actionEvent) {
+        Customer c1 = new Customer(txtId.getId(), txtName.getText(), txtAddress.getText(), Double.parseDouble(txtSalary.getText()));
+        boolean isSaved = Database.customerTable.add(c1);
+        if (isSaved){
+            new Alert(Alert.AlertType.INFORMATION,"Customer Saved!").show();
+            System.out.println("pressed");
+        }else {
+            new Alert(Alert.AlertType.WARNING,"Try Again!").show();
+        }
+    }
+
+
 }
