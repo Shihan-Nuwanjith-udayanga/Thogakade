22f84a663d65c889c721cf2188c5ffbdf2f44449
Shihan Nuwanjith <shihannuwanjith@gmail.com>
2023-08-03 13:45:11 +0530

    create a item arrayList

--- src/com/seekerscloud/pos/db/Database.java
+++ src/com/seekerscloud/pos/db/Database.java
@@ -1,9 +1,11 @@
 package com.seekerscloud.pos.db;
 
 import com.seekerscloud.pos.model.Customer;
+import com.seekerscloud.pos.model.Item;
 
 import java.util.ArrayList;
 
 public class Database {
     public static ArrayList<Customer> customerTable = new ArrayList<>();
+    public static ArrayList<Item> itemTable = new ArrayList<>();
 }
