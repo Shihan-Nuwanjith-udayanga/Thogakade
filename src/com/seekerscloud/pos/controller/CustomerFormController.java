10b5c54a62a3e0a51ceb39c4b660841983d06ce5
Shihan Nuwanjith <shihannuwanjith@gmail.com>
2023-08-03 10:25:12 +0530

    create delete option

--- src/com/seekerscloud/pos/controller/CustomerFormController.java
+++ src/com/seekerscloud/pos/controller/CustomerFormController.java
@@ -15,6 +15,7 @@ import javafx.stage.Stage;
 import javafx.stage.Window;
 
 import java.io.IOException;
+import java.util.Optional;
 
 public class CustomerFormController {
     public AnchorPane customerFormContext;
@@ -23,13 +24,13 @@ public class CustomerFormController {
     public TextField txtAddress;
     public TextField txtSalary;
     public TableView<CustomerTM> tblCustomer;
-    public TableColumn<Customer,String> colId;
-    public TableColumn<Customer,String> colName;
-    public TableColumn<Customer,String> colAddress;
-    public TableColumn<Customer,String> colSalary;
-    public TableColumn<Customer,Button> colOption;
+    public TableColumn<Customer, String> colId;
+    public TableColumn<Customer, String> colName;
+    public TableColumn<Customer, String> colAddress;
+    public TableColumn<Customer, String> colSalary;
+    public TableColumn<Customer, Button> colOption;
 
-    public void initialize(){
+    public void initialize() {
         colId.setCellValueFactory(new PropertyValueFactory<>("id"));
         colName.setCellValueFactory(new PropertyValueFactory<>("name"));
         colAddress.setCellValueFactory(new PropertyValueFactory<>("address"));
@@ -52,27 +53,43 @@ public class CustomerFormController {
     public void saveCustomerOnAction(ActionEvent actionEvent) {
         Customer c1 = new Customer(txtId.getText(), txtName.getText(), txtAddress.getText(), Double.parseDouble(txtSalary.getText()));
         boolean isSaved = Database.customerTable.add(c1);
-        if (isSaved){
+        if (isSaved) {
             searchCustomers();
             clearFields();
-            new Alert(Alert.AlertType.INFORMATION,"Customer Saved!").show();
-        }else {
-            new Alert(Alert.AlertType.WARNING,"Try Again!").show();
+            new Alert(Alert.AlertType.INFORMATION, "Customer Saved!").show();
+        } else {
+            new Alert(Alert.AlertType.WARNING, "Try Again!").show();
         }
     }
 
     private void searchCustomers() {
         ObservableList<CustomerTM> tmList = FXCollections.observableArrayList();
-        for (Customer c: Database.customerTable
-             ) {
+        for (Customer c : Database.customerTable
+        ) {
             Button btn = new Button("Delete");
-            CustomerTM tm = new CustomerTM(c.getId(),c.getName(),c.getAddress(),c.getSalary(),btn);
+            CustomerTM tm = new CustomerTM(c.getId(), c.getName(), c.getAddress(), c.getSalary(), btn);
             tmList.add(tm);
+
+            btn.setOnAction(event -> {
+                Alert alert = new Alert(Alert.AlertType.CONFIRMATION,
+                        "Are you sure whether do you want to delete this customer?",
+                        ButtonType.YES,ButtonType.NO);
+                Optional<ButtonType> buttonType = alert.showAndWait();
+                if (buttonType.get() == ButtonType.YES){
+                    boolean isDeleted = Database.customerTable.remove(c);
+                    if (isDeleted) {
+                        searchCustomers();
+                        new Alert(Alert.AlertType.INFORMATION, "Customer Deleted!").show();
+                    } else {
+                        new Alert(Alert.AlertType.WARNING, "Try Again!").show();
+                    }
+                }
+            });
         }
         tblCustomer.setItems(tmList);
     }
 
-    private void clearFields(){
+    private void clearFields() {
         txtId.clear();
         txtName.clear();
         txtAddress.clear();
