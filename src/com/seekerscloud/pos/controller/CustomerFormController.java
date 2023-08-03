dbb84927fe2c048a8dde6f248aca64fdfcd8ca84
Shihan Nuwanjith <shihannuwanjith@gmail.com>
2023-08-03 09:54:20 +0530

    create a searchCustomers table view

--- src/com/seekerscloud/pos/controller/CustomerFormController.java
+++ src/com/seekerscloud/pos/controller/CustomerFormController.java
@@ -2,12 +2,14 @@ package com.seekerscloud.pos.controller;
 
 import com.seekerscloud.pos.db.Database;
 import com.seekerscloud.pos.model.Customer;
+import com.seekerscloud.pos.view.tm.CustomerTM;
+import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.control.Alert;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 import javafx.stage.Window;
@@ -20,8 +22,22 @@ public class CustomerFormController {
     public TextField txtName;
     public TextField txtAddress;
     public TextField txtSalary;
+    public TableView<CustomerTM> tblCustomer;
+    public TableColumn<Customer,String> colId;
+    public TableColumn<Customer,String> colName;
+    public TableColumn<Customer,String> colAddress;
+    public TableColumn<Customer,String> colSalary;
+    public TableColumn<Customer,Button> colOption;
 
     public void initialize(){
+        colId.setCellValueFactory(new PropertyValueFactory<>("id"));
+        colName.setCellValueFactory(new PropertyValueFactory<>("name"));
+        colAddress.setCellValueFactory(new PropertyValueFactory<>("address"));
+        colSalary.setCellValueFactory(new PropertyValueFactory<>("salary"));
+        colOption.setCellValueFactory(new PropertyValueFactory<>("btn"));
+
+        searchCustomers();
+        clearFields();
     }
 
     private void setUi(String ui) throws IOException {
@@ -34,15 +50,32 @@ public class CustomerFormController {
     }
 
     public void saveCustomerOnAction(ActionEvent actionEvent) {
-        Customer c1 = new Customer(txtId.getId(), txtName.getText(), txtAddress.getText(), Double.parseDouble(txtSalary.getText()));
+        Customer c1 = new Customer(txtId.getText(), txtName.getText(), txtAddress.getText(), Double.parseDouble(txtSalary.getText()));
         boolean isSaved = Database.customerTable.add(c1);
         if (isSaved){
+            searchCustomers();
+            clearFields();
             new Alert(Alert.AlertType.INFORMATION,"Customer Saved!").show();
-            System.out.println("pressed");
         }else {
             new Alert(Alert.AlertType.WARNING,"Try Again!").show();
         }
     }
 
+    private void searchCustomers() {
+        ObservableList<CustomerTM> tmList = FXCollections.observableArrayList();
+        for (Customer c: Database.customerTable
+             ) {
+            Button btn = new Button("Delete");
+            CustomerTM tm = new CustomerTM(c.getId(),c.getName(),c.getAddress(),c.getSalary(),btn);
+            tmList.add(tm);
+        }
+        tblCustomer.setItems(tmList);
+    }
 
+    private void clearFields(){
+        txtId.clear();
+        txtName.clear();
+        txtAddress.clear();
+        txtSalary.clear();
+    }
 }
