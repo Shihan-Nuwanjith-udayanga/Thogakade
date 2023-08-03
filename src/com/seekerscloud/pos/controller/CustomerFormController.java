2e0713d249cebf01fcb5238413f9adceb90d0c81
Shihan Nuwanjith <shihannuwanjith@gmail.com>
2023-08-03 08:09:23 +0530

    create back to home button on action

--- src/com/seekerscloud/pos/controller/CustomerFormController.java
+++ src/com/seekerscloud/pos/controller/CustomerFormController.java
@@ -1,15 +1,32 @@
 package com.seekerscloud.pos.controller;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+import javafx.stage.Window;
+
+import java.io.IOException;
 
 public class CustomerFormController {
+    public AnchorPane customerFormContext;
     public TextField txtId;
     public TextField txtName;
     public TextField txtAddress;
     public TextField txtSalary;
 
+    private void setUi(String ui) throws IOException {
+        Stage stage = (Stage) customerFormContext.getScene().getWindow();
+        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("../view/"+ui+".fxml"))));
+    }
+
     public void saveCustomerOnAction(ActionEvent actionEvent) {
 
+    }
+
+    public void btnBackToHomeOnAction(ActionEvent actionEvent) throws IOException {
+        setUi("DashboardForm");
     }
 }
