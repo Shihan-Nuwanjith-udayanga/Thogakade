342323c848086899d3de0186d2ddf499e77e3af7
Shihan Nuwanjith <shihannuwanjith@gmail.com>
2023-08-10 14:50:51 +0530

    create static data from customer table and item table

--- src/com/seekerscloud/pos/db/Database.java
+++ src/com/seekerscloud/pos/db/Database.java
@@ -8,4 +8,18 @@ import java.util.ArrayList;
 public class Database {
     public static ArrayList<Customer> customerTable = new ArrayList<>();
     public static ArrayList<Item> itemTable = new ArrayList<>();
+
+    static {
+        customerTable.add(new Customer("C001","Shihan","Galle",85000));
+        customerTable.add(new Customer("C002","Kamal","Matara",45000));
+        customerTable.add(new Customer("C003","Amal","Colombo",25000));
+        customerTable.add(new Customer("C004","Nimal","Kaluthara",30000));
+        customerTable.add(new Customer("C005","Sunil","Galle",45000));
+
+        itemTable.add(new Item("I-001","Description 1",25,20));
+        itemTable.add(new Item("I-002","Description 2",35,50));
+        itemTable.add(new Item("I-003","Description 3",45,200));
+        itemTable.add(new Item("I-004","Description 4",20,70));
+        itemTable.add(new Item("I-005","Description 5",65,80));
+    }
 }
