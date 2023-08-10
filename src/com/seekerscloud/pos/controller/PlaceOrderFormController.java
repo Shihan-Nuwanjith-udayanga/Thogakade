7d41307ffa9d100a9a00d216cb857defbc6aaedd
Shihan Nuwanjith <shihannuwanjith@gmail.com>
2023-08-10 15:10:25 +0530

    load to the data from combobox

--- src/com/seekerscloud/pos/controller/PlaceOrderFormController.java
+++ src/com/seekerscloud/pos/controller/PlaceOrderFormController.java
@@ -1,18 +1,29 @@
 package com.seekerscloud.pos.controller;
 
+import com.seekerscloud.pos.db.Database;
+import com.seekerscloud.pos.model.Customer;
+import com.seekerscloud.pos.model.Item;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Objects;
 
 public class PlaceOrderFormController {
     public AnchorPane placeOrderFormContext;
     public TextField txtOrderId;
     public TextField txtDate;
-    public ComboBox cmbCustomerId;
+    public ComboBox<String> cmbCustomerId;
     public TextField txtName;
     public TextField txtAddress;
     public TextField txtSalary;
-    public ComboBox cmbItemCode;
+    public ComboBox<String> cmbItemCode;
     public TextField txtDescription;
     public TextField txtUnitPrice;
     public TextField txtQty;
@@ -27,11 +38,35 @@ public class PlaceOrderFormController {
     public Label lblTotal;
 
     public void initialize(){
-
+        setDateAndOrderId();
+        loadAllCustomerIds();
+        loadAllItemIds();
     }
 
-    public void backToHomeOnAction(ActionEvent actionEvent) {
+    private void loadAllItemIds() {
+        for(Item i:Database.itemTable){
+            cmbItemCode.getItems().add(i.getCode());
+        }
+    }
 
+    private void loadAllCustomerIds() {
+        for(Customer c:Database.customerTable){
+            cmbCustomerId.getItems().add(c.getId());
+        }
+    }
+
+    private void setDateAndOrderId() {
+        /* Date date = new Date();
+        SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd");
+        String d =df.format(date);
+        txtDate.setText(d);*/
+        txtDate.setText(new SimpleDateFormat("yyyy-MM-dd").format(new Date()));
+    }
+
+
+    public void backToHomeOnAction(ActionEvent actionEvent) throws IOException {
+        Stage stage = (Stage)placeOrderFormContext.getScene().getWindow();
+        stage.setScene(new Scene(FXMLLoader.load(Objects.requireNonNull(getClass().getResource("../view/DashboardForm.fxml")))));
     }
 
     public void addToCartOnAction(ActionEvent actionEvent) {
