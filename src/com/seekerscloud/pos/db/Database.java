b86e55523b5bc366c77f7a16edcdc91a992b37b2
Shihan Nuwanjith <shihannuwanjith@gmail.com>
2023-08-21 19:47:02 +0530

    create the dbconnect in the database class

--- src/com/seekerscloud/pos/db/Database.java
+++ src/com/seekerscloud/pos/db/Database.java
@@ -4,14 +4,33 @@ import com.seekerscloud.pos.model.Customer;
 import com.seekerscloud.pos.model.Item;
 import com.seekerscloud.pos.model.Order;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 public class Database {
+    private static Database database;
+    private final Connection connection;
+
+    private Database() throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.jdbc.Driver");
+        connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/thogakade","root","Shihan@1998");
+    }
+
+    public static Database getDatabase() throws SQLException, ClassNotFoundException {
+        return database == null ? new Database():database;
+    }
+
+    public Connection getConnection(){
+        return connection;
+    }
+
     public static ArrayList<Customer> customerTable = new ArrayList<>();
     public static ArrayList<Item> itemTable = new ArrayList<>();
     public static ArrayList<Order> orderTable = new ArrayList<>();
 
-    static {
+   /* static {
         customerTable.add(new Customer("C001","Shihan","Galle",85000));
         customerTable.add(new Customer("C002","Kamal","Matara",45000));
         customerTable.add(new Customer("C003","Amal","Colombo",25000));
@@ -23,5 +42,5 @@ public class Database {
         itemTable.add(new Item("I-003","Description 3",45,200));
         itemTable.add(new Item("I-004","Description 4",20,70));
         itemTable.add(new Item("I-005","Description 5",65,80));
-    }
+    }*/
 }
