14bd11f96a94d622cbe089c998c2c64c7417610f
Shihan Nuwanjith <shihannuwanjith@gmail.com>
2023-08-21 19:46:25 +0530

    update sql script

--- out/production/Thogakade/com/seekerscloud/pos/db/script.sql
+++ out/production/Thogakade/com/seekerscloud/pos/db/script.sql
@@ -4,18 +4,49 @@ CREATE DATABASE IF NOT EXISTS Thogakade;
 SHOW DATABASES;
 USE Thogakade;
 #===============
+DROP TABLE IF EXISTS Customer;
 CREATE TABLE IF NOT EXISTS Customer(
     id VARCHAR(45),
     name VARCHAR(45),
     address TEXT,
     salary DOUBLE,
-    CONSTRAINT PRIMARY KEY(id)
+    CONSTRAINT PRIMARY KEY (id)
 );
 #==============================
+DROP TABLE IF EXISTS Item;
 CREATE TABLE IF NOT EXISTS Item(
     code VARCHAR(45),
     description VARCHAR(250),
     unitPrice DOUBLE,
     qtyOnHand INT,
-    CONSTRAINT PRIMARY KEY(code)
+    CONSTRAINT PRIMARY KEY (code)
 );
+#==============================
+DROP TABLE IF EXISTS `Order`;
+CREATE TABLE IF NOT EXISTS `Order`(
+    oId  VARCHAR(45),
+    date DATE,
+    cost DOUBLE,
+    id  VARCHAR(15),
+    CONSTRAINT PRIMARY KEY (oId),
+    CONSTRAINT FOREIGN KEY (id) REFERENCES Customer (id)
+        ON UPDATE CASCADE ON DELETE CASCADE
+);
+SHOW TABLES;
+DESC `Order`;
+#=====================================
+DROP TABLE IF EXISTS `Order Details`;
+CREATE TABLE IF NOT EXISTS `Order Details`(
+    code VARCHAR(45),
+    oId VARCHAR(45),
+    unitPrice DOUBLE,
+    qty INT,
+    cost DOUBLE,
+    CONSTRAINT PRIMARY KEY (oId,code),
+    CONSTRAINT FOREIGN KEY (oId) REFERENCES `Order` (oId)
+        ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT FOREIGN KEY (code) REFERENCES `Item` (code)
+        ON DELETE CASCADE ON UPDATE CASCADE
+);
+SHOW TABLES;
+DESC `Order Details`;
